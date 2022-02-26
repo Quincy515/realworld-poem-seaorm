0e6e7713322622087991eeda33a3de158f527b06
CusterFun <custer@email.cn>
2022-02-27 01:14:16 +0800

    poem hello world

---
+++ server/src/main.rs
@@ -0,0 +1,30 @@
+use poem::{Server, listener::TcpListener, Route};
+use poem_openapi::{OpenApi, payload::PlainText, param::Query, OpenApiService};
+
+struct Api;
+
+#[OpenApi]
+impl Api {
+    #[oai(path = "/hello", method = "get")]
+    async fn index(&self, name: Query<Option<String>>) -> PlainText<String> {
+        match name.0 {
+            Some(name) => PlainText(format!("hello, {}", name)),
+            None => PlainText("hello!".to_string()),
+        }
+    }
+}
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    if std::env::var_os("RUST_LOG").is_none() {
+        std::env::set_var("RUST_LOG", "poem=debug");
+    }
+    tracing_subscriber::fmt::init();
+
+    let api_service =
+        OpenApiService::new(Api, "Hello World", "1.0").server("http://localhost:3000/api");
+    let ui = api_service.swagger_ui();
+
+    Server::new(TcpListener::bind("127.0.0.1:3000"))
+        .run(Route::new().nest("/api", api_service).nest("/", ui))
+        .await
+}
